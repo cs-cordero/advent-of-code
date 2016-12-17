45869e0a2f0588b7d11a35766f1557afa1e34ac1
Christopher Cordero <chris.s.cordero@gmail.com>
2016-12-17 15:11:50 -0500

    PEP8 adjustments

--- day17/aoc_day_17.py
+++ day17/aoc_day_17.py
@@ -6,7 +6,7 @@ from hashlib import md5
 
 def day17(hashkey):
     queue = [(hashkey, (0, 0))]
-    directions = {0:'U', 1:'D', 2:'L', 3:'R'}
+    directions = {0: 'U', 1: 'D', 2: 'L', 3: 'R'}
     longest_path_length = 0
     shortest_path = None
     while queue:
@@ -28,8 +28,8 @@ def day17(hashkey):
         for i in range(4):
             x, y = candidates[i]
             h = md5(next_instructions).hexdigest()[:4]
-            if ((x < 0 or y < 0 or x > 3 or y > 3)
-                or not h[i] in ('b','c','d','e','f')):
+            if ((x < 0 or y < 0 or x > 3 or y > 3) or
+               not h[i] in ('b', 'c', 'd', 'e', 'f')):
                 continue
             new_hash = next_instructions + directions[i]
             queue.append((new_hash, candidates[i]))

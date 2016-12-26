c6f45af9145c43266632491964091302303e33b6
Christopher Cordero <chris.s.cordero@gmail.com>
2016-12-26 15:53:55 -0500

    Completed Day 24 with O(N!) solution =(

--- day24/aoc_day_24.py
+++ day24/aoc_day_24.py
@@ -2,7 +2,7 @@
 
 import copy
 import collections
-import re
+from itertools import permutations
 
 
 def enqueue(grid, x, y, queue, steps):
@@ -44,36 +44,6 @@ def get_distances(grid, x, y):
     return distances, grid
 
 
-def find_min(d, visited):
-    curr_key = None
-    for key in d:
-        if key in visited:
-            continue
-        if not curr_key or (curr_key and d[curr_key] > d[key]):
-            curr_key = key
-    return curr_key
-
-
-def find_path(d, current_key, visited, steps = 0):
-    visited = copy.deepcopy(visited)
-    while len(visited) < len(d):
-        step_count = 0
-        min_dist = min(x[1] for x in d[current_key].items() \
-                       if x[0] not in visited)
-        candidates = [key[0] for key in d[current_key].items() \
-                     if key[1] == min_dist]
-        if len(candidates) > 1:
-            dists = []
-            for candidate in candidates:
-                dists.append((candidate, find_path(d, candidate, visited, d[current_key][candidate])))
-            print(dists)
-            return steps + min(dist[1] for dist in dists)
-        steps += d[current_key][candidates[0]]
-        current_key = candidates[0]
-        visited.add(candidates[0])
-    return steps
-
-
 def main():
     grid = []
     targets = {}
@@ -98,15 +68,33 @@ def main():
             x += 1
 
     for key, val in target_locs.items():
-        targets[key], grid = get_distances(grid, val[0], val[1])
-
-    
-    for key, val in targets.items():
-        print(key, targets[key])
+        targets[key], grid = get_distances(grid, val[0], val[1]) 
 
     visited = set('0')
-    print(find_path(targets, '0', visited))
+    test = []
+    for target in targets:
+        print(target, targets[target])
+
+    part_one = []
+    part_two = []
+    perms = [list(perm) for perm in permutations(range(8), 8)]
+    
+    for perm in perms:
+        new = sum(targets[str(perm[i-1])][str(perm[i])]
+                  for i in range(1, len(perm)))
+        part_one.append(new)
+
+        if perm[0] == 0:
+            if perm[-1]:
+                perm.append(0)
+            new = sum(targets[str(perm[i-1])][str(perm[i])]
+                      for i in range(1, len(perm)))
+            part_two.append(new)
+
+
+    print('Part 1: %d' % min(part_one))
+    print('Part 2: %d' % min(part_two))
 
 
 if __name__ == '__main__':
-    print(main())
+    main()

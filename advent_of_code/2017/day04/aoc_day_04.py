a8ea2103dbd2b2968e991d0f359ad1775d05d629
Christopher Cordero <chris.s.cordero@gmail.com>
2017-12-05 00:18:02 -0500

    completed day 4

---
+++ advent_of_code/2017/day04/aoc_day_04.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+
+def solution(lines):
+    valid = 0
+    for line in lines:
+        line = line.strip().split(' ')
+        seen = set()
+        for word in line:
+            if word in seen:
+                break
+            for perm in ("".join(s) for s in permutations(word)):
+                seen.add(perm)
+        else:
+            valid += 1
+    return valid
+
+
+
+if __name__ == '__main__':
+    with open('aoc_day_04_input.txt') as f:
+        s = f.readlines()
+    print(solution(s))

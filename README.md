40d3979fb15fb0391cf684018b2de2e71776a710
Christopher Cordero <chris.s.cordero@gmail.com>
2017-12-09 12:02:48 -0500

    Completed Day 9

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ Day 05: [A Maze of Twisty Trampolines, All Alike](http://adventofcode.com/2017/d
 Day 06: [Memory Reallocation](http://adventofcode.com/2017/day/6) | \*\*
 Day 07: [Recursive Circus](http://adventofcode.com/2017/day/7) | \*\*
 Day 08: [I Heard You Like Registers](http://adventofcode.com/2017/day/8) | \*\*
-Day 09: [???](http://adventofcode.com/2017/day/9) |
+Day 09: [Stream Processing](http://adventofcode.com/2017/day/9) | \*\*
 Day 10: [???](http://adventofcode.com/2017/day/10) |
 Day 11: [???](http://adventofcode.com/2017/day/11) |
 Day 12: [???](http://adventofcode.com/2017/day/12) |
---
+++ advent_of_code/2017/day09/aoc_day_09.py
@@ -0,0 +1,41 @@
+def solution(stream):
+    score = 0
+    last_score = 0
+    garbage_score = 0
+
+    flag_bang = False
+    flag_garbage = False
+
+    for char in stream:
+        if flag_bang:
+            # ignore the character if it's being escaped
+            flag_bang = False
+            continue
+
+        if char == '>':
+            # unflag the garbage flag if encounter unescaped >
+            flag_garbage = False
+        elif char == '!':
+            # flag the ignore flag if encounter unescaped !
+            flag_bang = True
+        elif flag_garbage:
+            # as long as the garbage flag is flagged, keep skipping
+            garbage_score += 1
+            continue
+        elif char == '{':
+            last_score += 1
+        elif char == '}':
+            score += last_score
+            last_score -= 1
+        elif char == '<':
+            flag_garbage = True
+    return score, garbage_score
+
+
+
+if __name__ == '__main__':
+    with open('aoc_day_09_input.txt') as f:
+        s = [line.strip() for line in f.readlines()][0]
+    answer = solution(s)
+    print(f'Part 1: {answer[0]}')
+    print(f'Part 2: {answer[1]}')

90117b9f9eb9991c3877ebc615767c04c46b7c47
Christopher Cordero <chris.s.cordero@gmail.com>
2018-12-16 01:13:19 -0500

    completed day 9

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Day 05: [Alchemical Reduction](http://adventofcode.com/2018/day/5) | \*\*
 Day 06: [Chronal Coordinates](http://adventofcode.com/2018/day/6) | \*\*
 Day 07: [The Sum of Its Parts](http://adventofcode.com/2018/day/7) | \*\*
 Day 08: [Memory Maneuver](http://adventofcode.com/2018/day/8) | \*\*
-Day 09: [---](http://adventofcode.com/2018/day/9) |
+Day 09: [Marble Mania](http://adventofcode.com/2018/day/9) | \*\*
 Day 10: [---](http://adventofcode.com/2018/day/10) |
 Day 11: [---](http://adventofcode.com/2018/day/11) |
 Day 12: [---](http://adventofcode.com/2018/day/12) |
---
+++ advent_of_code/2018/day09/day09.py
@@ -0,0 +1,31 @@
+from collections import deque, defaultdict
+
+
+def solution(players, last_marble):
+    current_player = 0
+    current_marble = 1
+    scores = defaultdict(int)
+    board = deque([0])
+    while current_marble <= last_marble:
+        if current_marble % 23 == 0:
+            scores[current_player] += current_marble
+            board.rotate(-7)
+            scores[current_player] += board.popleft()
+            board.rotate(1)
+        else:
+            board.rotate(1)
+            board.appendleft(current_marble)
+
+        current_marble += 1
+        current_player = (current_player + 1) % players
+
+    return max(scores.values())
+
+
+assert solution(10, 1618) == 8317
+assert solution(13, 7999) == 146373
+assert solution(17, 1104) == 2764
+assert solution(21, 6111) == 54718
+assert solution(30, 5807) == 37305
+print(solution(476, 71431))
+print(solution(476, 71431*100))

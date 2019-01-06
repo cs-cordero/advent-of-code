a50792b05513df05a8f782a314566740592ee769
Christopher Cordero <ccordero@protonmail.com>
2019-01-06 15:57:47 -0500

    cleanup

--- advent_of_code/2018/day24/day24.py
+++ advent_of_code/2018/day24/day24.py
@@ -1,4 +1,4 @@
-from typing import Set, Dict, Optional, List, Tuple
+from typing import Set, Dict, Optional, List, Tuple, Union
 import enum
 import operator
 import re
@@ -63,7 +63,7 @@ class Group:
         return f'Group {self.per_army_group_id} contains {self.unit_count} units.'
 
 
-def get_groups():
+def get_groups(boost: int = 0):
     def read_file(filepath):
         if 'test' not in sys.argv:
             with open(filepath) as f:
@@ -79,14 +79,6 @@ def get_groups():
                 '801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1',
                 '4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4',
             ]
-            # TEST_DATA = [
-            #     'Immune System:',
-            #     '10 units each with 110 hit points with an attack that does 50 fire damage at initiative 9',
-            #     '',
-            #     '102 units each with 100 hit points with an attack that does 1 bludgeoning damage at initiative 1',
-            #     '101 units each with 100 hit points with an attack that does 1 slashing damage at initiative 4',
-            # ]
-
             for line in TEST_DATA:
                 yield line
 
@@ -131,16 +123,23 @@ def get_groups():
 
         immunities, weaknesses = get_immunities_and_weaknesses(match_groups['resistances'])
         attack_type = AttackType(match_groups['attack_type'])
-        data = {
+        attack_dmg = int(match_groups['attack_dmg'])
+        attack_dmg += boost if army == Army.IMMUNE else 0
+        unit_count = int(match_groups['unit_count'])
+        per_unit_hp = int(match_groups['per_unit_hp'])
+        initiative = int(match_groups['initiative'])
+        data: Dict[str, Union[Army, AttackType, Set[AttackType], int]] = {
             'army': army,
             'per_army_group_id': per_army_group_count,
             'immunities': immunities,
             'weaknesses': weaknesses,
+            'attack_dmg': attack_dmg,
             'attack_type': attack_type,
-            **{key: int(value) for key, value in match_groups.items()
-               if key in ['unit_count', 'per_unit_hp', 'attack_dmg', 'initiative']}
+            'unit_count': unit_count,
+            'per_unit_hp': per_unit_hp,
+            'initiative': initiative,
         }
-        groups[overall_group_count] = Group(**data)
+        groups[overall_group_count] = Group(**data)  # type: ignore
         per_army_group_count += 1
         overall_group_count += 1
     return groups
@@ -180,6 +179,7 @@ def target_selection_phase(groups: Dict[int, Group],
             if current_group.attack_type in target_group.weaknesses:
                 potential_damage *= 2
 
+            # This block is dirty but guaranteed correct.
             if current_target is None:
                 current_target = target_group
                 current_target_id = target_group_id
@@ -209,7 +209,7 @@ def target_selection_phase(groups: Dict[int, Group],
 
 def damage_phase(groups: Dict[int, Group],
                  damage_selection_order: List[int],
-                 attacker_to_defender_map: Dict[int, int]) -> None:
+                 attacker_to_defender_map: Dict[int, Optional[int]]) -> None:
     for attacker_id in damage_selection_order:
         attacker = groups[attacker_id]
 
@@ -228,8 +228,8 @@ def damage_phase(groups: Dict[int, Group],
         defender.takes(damage)
 
 
-def solution():
-    groups = get_groups()
+def solution(boost: int = 0):
+    groups = get_groups(boost)
 
     armies = {
         Army.IMMUNE: [group for group in groups.values()
@@ -248,10 +248,43 @@ def solution():
     while all(any(group.is_alive for group in army) for army in armies.values()):
         target_selection_order = sorted(groups, key=target_selection_func, reverse=True)
         attacker_to_defender_map = target_selection_phase(groups, target_selection_order)
+        pre_total_units = sum(group.unit_count for group_id, group in groups.items())
         damage_phase(groups, damage_selection_order, attacker_to_defender_map)
+        post_total_units = sum(group.unit_count for group_id, group in groups.items())
+        if pre_total_units == post_total_units:
+            # no damage was dealt, so this will be a tie forever
+            return post_total_units, 'TIED!'
 
     part1 = sum(group.unit_count for group_id, group in groups.items())
-    return part1
+    winner = (Army.IMMUNE
+              if any(group.hp > 0 for group in armies[Army.IMMUNE])
+              else Army.INFECTION)
+    return part1, winner
 
 
-print(solution())
+def solution2():
+    # find a boost value that allows the IMMUNE army to win
+    boost = 1000  # arbitrary starting point
+    left = 0
+    while True:
+        _, winner = solution(boost)
+        if winner == Army.IMMUNE:
+            right = boost
+            break
+        else:
+            left = boost
+            boost *= 2
+
+    while left < right:
+        boost = (left + right) // 2
+        _, winner = solution(boost)
+        if winner == Army.IMMUNE:
+            right = boost
+        else:
+            left = boost + 1
+    winning_boost = boost + 1 if winner != Army.IMMUNE else boost
+    return solution(winning_boost)[0]
+
+
+print(f'Part 1: {solution(0)[0]}')
+print(f'Part 2: {solution2()}')

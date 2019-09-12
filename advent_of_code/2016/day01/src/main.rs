9258e34fa96fe536092b769ecd52cc8f14bdc203
Christopher Cordero <ccordero@protonmail.com>
2019-09-11 23:04:01 -0400

    completed day 1 2016 in rust

--- advent_of_code/2016/day01/src/main.rs
+++ advent_of_code/2016/day01/src/main.rs
@@ -1,12 +1,15 @@
+use std::collections::HashSet;
 use std::fmt;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
 
-
 fn main() -> io::Result<()> {
     let steps = get_steps("src/input.txt")?;
     let mut current_direction = Direction::North;
+
+    let mut visited: HashSet<(i32, i32)> = HashSet::new();
+    let mut first_return: Option<(i32, i32)> = None;
 
     let mut x = 0;
     let mut y = 0;
@@ -22,15 +25,26 @@ fn main() -> io::Result<()> {
         }
 
         let movement = step.get(1..).unwrap().parse::<i32>().unwrap();
-        match current_direction {
-            Direction::North => x += movement,
-            Direction::South => x -= movement,
-            Direction::East => y += movement,
-            Direction::West => y -= movement,
+        for _i in 0..movement {
+            match current_direction {
+                Direction::North => x += 1,
+                Direction::South => x -= 1,
+                Direction::East => y += 1,
+                Direction::West => y -= 1,
+            };
+            let current_position = (x, y);
+            if first_return == None && visited.contains(&current_position) {
+                first_return = Some(current_position);
+            };
+            visited.insert(current_position);
         }
     }
     println!("Position: {} ({}, {})", current_direction, x, y);
     println!("Part 1 Answer: {}", x.abs() + y.abs());
+    println!(
+        "Part 2 Answer: {:?}",
+        first_return.unwrap().0.abs() + first_return.unwrap().1.abs()
+    );
     return Ok(());
 }
 
@@ -47,7 +61,7 @@ impl Direction {
             Direction::North => Direction::West,
             Direction::West => Direction::South,
             Direction::South => Direction::East,
-            Direction::East => Direction::North
+            Direction::East => Direction::North,
         };
     }
     pub fn turn_right(&self) -> Direction {
@@ -55,7 +69,7 @@ impl Direction {
             Direction::North => Direction::East,
             Direction::West => Direction::North,
             Direction::South => Direction::West,
-            Direction::East => Direction::South
+            Direction::East => Direction::South,
         };
     }
 }
@@ -73,7 +87,11 @@ impl fmt::Display for Direction {
 
 fn get_steps(path: &str) -> io::Result<Vec<String>> {
     let contents = read_input(path)?;
-    let steps = contents.trim_end().split(", ").map(|s| s.to_string()).collect();
+    let steps = contents
+        .trim_end()
+        .split(", ")
+        .map(|s| s.to_string())
+        .collect();
     return Ok(steps);
 }
 

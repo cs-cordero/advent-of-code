85cc9add168df3a22a70e1586e6b79db018f68b0
Christopher Cordero <ccordero@protonmail.com>
2020-12-03 01:15:45 -0500

    Complete 2020 Day 3

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ Challenge | Status
 --- | :---:
 Day 01: [Report Repair](http://adventofcode.com/2020/day/1) | ⭐⭐
 Day 02: [Password Philosophy](http://adventofcode.com/2020/day/2) | ⭐⭐
-Day 03: [???](http://adventofcode.com/2020/day/3) |
+Day 03: [Toboggan Trajectory](http://adventofcode.com/2020/day/3) | ⭐⭐
 Day 04: [???](http://adventofcode.com/2020/day/4) |
 Day 05: [???](http://adventofcode.com/2020/day/5) |
 Day 06: [???](http://adventofcode.com/2020/day/6) |
---
+++ rs/2020/day03/src/main.rs
@@ -0,0 +1,25 @@
+use advent_of_code::*;
+
+fn main() {
+    let lines = read_input_as_lines("2020/day03/src/input.txt");
+
+    let answer1 = count_trees(&lines, 3, 1);
+    let answer2 = &[(1_usize, 1_usize), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|(right, down)| count_trees(&lines, *right, *down))
+        .fold(1, |acc, trees| acc * trees);
+
+    println!("Part 1: {}", answer1);
+    println!("Part 2: {}", answer2);
+}
+
+fn count_trees(grid: &Vec<String>, right: usize, down: usize) -> usize {
+    grid
+        .iter()
+        .enumerate()
+        .skip(down)
+        .filter(|(row, line)| {
+            row % down == 0 && line.chars().nth((row * right / down) % line.len()).unwrap() == '#'
+        })
+        .count()
+}

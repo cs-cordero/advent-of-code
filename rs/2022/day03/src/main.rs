1dca3c97b1425e64de59e1052a6fe6ca8c606368
Christopher Cordero <ccordero@protonmail.com>
2022-12-03 01:24:31 -0500

    Completed 2022 Day 3

---
+++ rs/2022/day03/src/main.rs
@@ -0,0 +1,46 @@
+use std::collections::HashSet;
+use advent_of_code::*;
+
+fn main() {
+    let input = read_input_as_lines("2022/day03/src/input.txt");
+
+    let part1: i32 = input
+        .iter()
+        .map(|rucksack| {
+            let length = rucksack.len();
+
+            let (left, right) = rucksack.split_at(length / 2);
+            let left = left.chars().collect::<HashSet<_>>();
+            let right = right.chars().collect::<HashSet<_>>();
+
+            find_priority(left.intersection(&right).collect())
+        })
+        .sum();
+
+    let part2: i32 = input
+        .chunks_exact(3)
+        .map(|rucksack| {
+            let elf1 = rucksack[0].chars().collect::<HashSet<_>>();
+            let elf2 = rucksack[1].chars().collect::<HashSet<_>>();
+            let elf3 = rucksack[2].chars().collect::<HashSet<_>>();
+            find_priority(elf1.intersection(&elf2.intersection(&elf3).copied().collect()).collect())
+        })
+        .sum();
+
+    println!("Part 1: {:?}", part1);
+    println!("Part 2: {:?}", part2);
+}
+
+fn find_priority(items: HashSet<&char>) -> i32 {
+    items.into_iter()
+        .copied()
+        .fold(0, |acc, item| {
+            if item.is_lowercase() {
+                acc + item as i32 - 96
+            } else if item.is_uppercase() {
+                acc + item as i32 - 64 + 26
+            } else {
+                panic!("Failed to find priority of item");
+            }
+        })
+}
